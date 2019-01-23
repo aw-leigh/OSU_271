02c5e7d51b14ddbfa024077210792c62c5797db1
aw-leigh <puddingisfun@gmail.com>
2019-01-23 13:44:43 +0900

    Renamed template, Project02 asm

---
+++ Project00_wilsoan6.asm
@@ -0,0 +1,28 @@
+TITLE INSERT HERE    (Project00 INSERT HERE .asm)
+
+; Author: Andrew Wilson
+; Last Modified: INSERT HERE
+; OSU email address: wilsoan6@oregonstate.edu
+; Course number/section: 271-400
+; Project Number: INSERT HERE                Due Date: INSERT HERE
+; Description: INSERT HERE
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
